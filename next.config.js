b580e9ec70562bcfdeb628ae9d95303420b78414
Zonotora <zonotora@hotmail.com>
2022-12-25 21:46:58 +0100

    Add backup redirection

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
       {
         source: "/",
         destination: "/posts",
-        permanent: true,
+        permanent: false,
       },
     ];
   },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import Router from "next/router";
 import Header from "../components/header";
 
 export const Home = () => {
+  useEffect(() => {
+    const { pathname } = Router;
+
+    if (pathname == "/") {
+      location.replace("/posts");
+    }
+  }, []);
+
   return (
     <div className="main">
       <Header active="" />
