6cf6f318e8824ff715e0ea0dc65dd18f25c8826d
Zonotora <zonotora@hotmail.com>
2024-12-05 22:47:06 +0100

    Add content to docs page

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,10 +1,41 @@
 import Header from "../../components/header";
+import Tag from "../../components/tag";
+import Link from "next/link";
+
+const docs = [
+  {
+    title: "guidos",
+    description: "Operating system from scratch.",
+    tags: ["os", "c", "assembly", "x86"],
+    link: "https://zonotora.github.io/guidos/book/index.html",
+  },
+];
 
 export const Home = () => {
   return (
     <div className="main">
       <Header active="docs" />
-      WIP
+      <div className="posts">
+        {docs.map((doc) => (
+          <Link
+            key={doc.title}
+            href={doc.link}
+            className="project-preview"
+            style={{ cursor: "pointer" }}
+          >
+            <div style={{ position: "absolute", right: "0" }}></div>
+            <div className="project-preview-title">
+              <b>{doc.title}</b>
+            </div>
+            <div className="project-preview-info tags">
+              {doc.tags.map((tag) => (
+                <Tag key={tag} name={tag} />
+              ))}
+            </div>
+            <div className="project-preview-content">{doc.description}</div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
