7d5905b42df0b7cbd7e95bf25c7efe91532102ab
Zonotora <zonotora@hotmail.com>
2025-02-05 23:41:02 +0100

    Add publication

--- pages/publications/index.tsx
+++ pages/publications/index.tsx
@@ -1,7 +1,26 @@
+import ProjectPreview from "../../components/project-preview";
 import Page from "../../components/page";
+import publications from "../../data/publications.json";
 
 export const Home = () => {
-  return <Page active="publications">WIP</Page>;
+  return (
+    <Page active="publications">
+      {" "}
+      <div className="posts">
+        {publications.map((publication) => (
+          <ProjectPreview
+            key={publication.title}
+            title={publication.title}
+            description={publication.conference}
+            tags={publication.authors}
+            link=""
+            githubLink=""
+            archived={false}
+          />
+        ))}
+      </div>
+    </Page>
+  );
 };
 
 export default Home;
