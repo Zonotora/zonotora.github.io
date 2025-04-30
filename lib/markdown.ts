a5e6de373ab23f8b4d96484780ee0fa9942342f3
Zonotora <zonotora@hotmail.com>
2025-04-30 14:16:37 +0200

    Add mdx

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -4,6 +4,7 @@ import rehypeSlug from "rehype-slug";
 import rehypeToc from "rehype-toc";
 import remarkMath from "remark-math";
 import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import remarkPrism from "remark-prism";
 import { unified } from "unified";
@@ -11,6 +12,7 @@ import { unified } from "unified";
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
+    .use(remarkGfm)
     .use(remarkMath)
     .use(remarkPrism)
     .use(remarkRehype)
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
+import rehypeToc from "rehype-toc";
+import remarkMath from "remark-math";
+import remarkGfm from "remark-gfm";
+import remarkPrism from "remark-prism";
+import mdx from "@next/mdx";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -17,4 +25,14 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = mdx({
+  options: {
+    remarkPlugins: [remarkGfm, remarkMath, remarkPrism],
+    rehypePlugins: [rehypeKatex, rehypeSlug, rehypeToc],
+  },
+});
+
+export default withMDX({
+  ...nextConfig,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+});
