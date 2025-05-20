ade56978fe9beca929bba2b412e05efbce52faf7
Zonotora <zonotora@hotmail.com>
2025-05-20 14:16:49 +0200

    Temporary Heatmap

--- components/heatmap.tsx
+++ components/heatmap.tsx
@@ -1,61 +1,62 @@
-const Heatmap = ({ data }) => {
-  return (
-    <div>
-      <table className="heatmap">
-        <tbody>
-          <tr>
-            {Array(52)
-              .fill(0)
-              .map((o, i) => (
-                <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
-              ))}
-          </tr>
-          <tr>
-            {Array(52)
-              .fill(0)
-              .map((o, i) => (
-                <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
-              ))}
-          </tr>
-          <tr>
-            {Array(52)
-              .fill(0)
-              .map((o, i) => (
-                <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
-              ))}
-          </tr>
-          <tr>
-            {Array(52)
-              .fill(0)
-              .map((o, i) => (
-                <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
-              ))}
-          </tr>
-          <tr>
-            {Array(52)
-              .fill(0)
-              .map((o, i) => (
-                <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
-              ))}
-          </tr>
-          <tr>
-            {Array(52)
-              .fill(0)
-              .map((o, i) => (
-                <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
-              ))}
-          </tr>
-          <tr>
-            {Array(51)
-              .fill(0)
-              .map((o, i) => (
-                <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
-              ))}
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
+export const Heatmap = 0;
+// const Heatmap = ({ data }) => {
+//   return (
+//     <div>
+//       <table className="heatmap">
+//         <tbody>
+//           <tr>
+//             {Array(52)
+//               .fill(0)
+//               .map((o, i) => (
+//                 <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
+//               ))}
+//           </tr>
+//           <tr>
+//             {Array(52)
+//               .fill(0)
+//               .map((o, i) => (
+//                 <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
+//               ))}
+//           </tr>
+//           <tr>
+//             {Array(52)
+//               .fill(0)
+//               .map((o, i) => (
+//                 <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
+//               ))}
+//           </tr>
+//           <tr>
+//             {Array(52)
+//               .fill(0)
+//               .map((o, i) => (
+//                 <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
+//               ))}
+//           </tr>
+//           <tr>
+//             {Array(52)
+//               .fill(0)
+//               .map((o, i) => (
+//                 <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
+//               ))}
+//           </tr>
+//           <tr>
+//             {Array(52)
+//               .fill(0)
+//               .map((o, i) => (
+//                 <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
+//               ))}
+//           </tr>
+//           <tr>
+//             {Array(51)
+//               .fill(0)
+//               .map((o, i) => (
+//                 <td style={{ backgroundColor: `#00${i % 10}0f1` }}>{}</td>
+//               ))}
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
 
-export default Heatmap;
+// export default Heatmap;
